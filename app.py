3e9337be4600680a8c3085944faae1e147057bda
otonye77 <otonye.amietubodie@decagon.dev>
2023-03-27 20:37:42 +0100

    Did the post route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 stores = [
@@ -16,3 +16,10 @@ stores = [
 @app.get("/store")
 def get_stores():
     return {"stores": stores}
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    return new_store, 201
